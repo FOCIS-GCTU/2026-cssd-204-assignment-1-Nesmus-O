bbe34d538476970b2aa8a400d7ff7b47d6daadf8
Nesmus-O <onesimusbotweampah20@gmail.com>
2026-05-31 21:04:08 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Onesimus Botwe Ampah
+# STUDENT ID:  2425400623
+# Email: onesimusbotweampah20@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Onesimus Botwe Ampah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials OBA in stylized large block letters, each 12 characters wide by 10 characters high,
+    # followed by a large period of  asterisks, with
+    # three dot as separators.
 
+    print()
+    print("...OBA")
+    print()
+    print("....OOOOOOOOOO.........BBBBBBBBB................AA..........")
+    print("...OO........OO........BB.......BB.............AAAA.........")
+    print("...OO........OO........BB........BB...........AA..AA........")
+    print("...OO........OO........BB.......BB...........AA....AA.......")
+    print("...OO........OO........BBBBBBBBB............AA......AA......")
+    print("...OO........OO........BBBBBBBBB...........AAAAAAAAAAAA.....")
+    print("...OO........OO........BB.......BB.........AA........AA.....")
+    print("...OO........OO........BB........BB........AA........AA.....")
+    print("...OO........OO...**...BB.......BB....**...AA........AA...**")
+    print("....OOOOOOOOOO....**...BBBBBBBBB......**...AA........AA...**")
+    print()
 
 
 main()
